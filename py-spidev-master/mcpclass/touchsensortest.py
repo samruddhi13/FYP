538372a295e3e2e534e261ddae23654c3da0aa0f
samruddhi13 <samruddhi13@gmail.com>
2022-11-24 06:33:51 +0000

    sensor data reading format

--- py-spidev-master/mcpclass/touchsensortest.py
+++ py-spidev-master/mcpclass/touchsensortest.py
@@ -1,14 +1,23 @@
 from MCP3008 import MCP3008
+#import keyboard 
+import time
 
 thislist = []
 
 adc = MCP3008()
-for i in range(6):
 
-    value = int(adc.read( channel = i )) # You can of course adapt the channel to be read out
-    processedvalue =  round((value / 1023.0 * 3.3) ,2)
-    thislist.insert(i, processedvalue)
+while True: 
+	for i in range(6):
 
-#print("Applied voltage: %.2f" % (value / 1023.0 * 3.3) )
+	    value = int(adc.read( channel = i )) # You can of course adapt the channel to be read out
+	    processedvalue =  round((value / 1023.0 * 3.3) ,2)
+	    thislist.insert(i, value)
 
-print(thislist)
+	#print("Applied voltage: %.2f" % (value / 1023.0 * 3.3) )
+
+	print(thislist)
+	thislist = []
+	time.sleep(2)
+	#if keyboard.is_pressed("q"):
+	#	print("q pressed, loop ending")
+	#	break
